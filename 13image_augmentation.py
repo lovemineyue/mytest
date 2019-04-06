5e78ecd7480fbb9b8d4158fded0d236fae2f558e
cp@datasoftcn.com <peng@qq.com>
2019-04-06 12:26:20 +0800

    .....

--- 13image_augmentation.py
+++ 13image_augmentation.py
@@ -51,3 +51,95 @@ augs = gdata.vision.transforms.Compose([
 # plt.show()
 
 show_images(gdata.vision.CIFAR10(train=True)[0:32][0], 4, 8, scale=0.8)
+
+
+flip_aug = gdata.vision.transforms.Compose([
+    gdata.vision.transforms.RandomFlipLeftRight(),
+    gdata.vision.transforms.ToTensor()])
+
+no_aug = gdata.vision.transforms.Compose([
+    gdata.vision.transforms.ToTensor()])
+
+
+num_workers = 0 if sys.platform.startswith('win32') else 4
+def load_cifar10(is_train, augs, batch_size):
+    return gdata.DataLoader(
+        gdata.vision.CIFAR10(train=is_train).transform_first(augs),
+        batch_size=batch_size, shuffle=is_train, num_workers=num_workers)
+
+
+def try_all_gpus():  # 本函数已保存在d2lzh包中方便以后使用
+    ctxes = []
+    try:
+        for i in range(16):  # 假设一台机器上GPU的数量不超过16
+            ctx = mx.gpu(i)
+            _ = nd.array([0], ctx=ctx)
+            ctxes.append(ctx)
+    except mx.base.MXNetError:
+        pass
+    if not ctxes:
+        ctxes = [mx.cpu()]
+    return ctxes
+
+def _get_batch(batch, ctx):
+    features, labels = batch
+    if labels.dtype != features.dtype:
+        labels = labels.astype(features.dtype)
+    return (gutils.split_and_load(features, ctx),
+            gutils.split_and_load(labels, ctx), features.shape[0])
+
+
+# 本函数已保存在d2lzh包中方便以后使用
+def evaluate_accuracy(data_iter, net, ctx=[mx.cpu()]):
+    if isinstance(ctx, mx.Context):
+        ctx = [ctx]
+    acc_sum, n = nd.array([0]), 0
+    for batch in data_iter:
+        features, labels, _ = _get_batch(batch, ctx)
+        for X, y in zip(features, labels):
+            y = y.astype('float32')
+            acc_sum += (net(X).argmax(axis=1) == y).sum().copyto(mx.cpu())
+            n += y.size
+        acc_sum.wait_to_read()
+    return acc_sum.asscalar() / n
+
+# 本函数已保存在d2lzh包中方便以后使用
+def train(train_iter, test_iter, net, loss, trainer, ctx, num_epochs):
+    print('training on', ctx)
+    if isinstance(ctx, mx.Context):
+        ctx = [ctx]
+    for epoch in range(num_epochs):
+        train_l_sum, train_acc_sum, n, m, start = 0.0, 0.0, 0, 0, time.time()
+        for i, batch in enumerate(train_iter):
+            Xs, ys, batch_size = _get_batch(batch, ctx)
+            ls = []
+            with autograd.record():
+                y_hats = [net(X) for X in Xs]
+                ls = [loss(y_hat, y) for y_hat, y in zip(y_hats, ys)]
+            for l in ls:
+                l.backward()
+            trainer.step(batch_size)
+            train_l_sum += sum([l.sum().asscalar() for l in ls])
+            n += sum([l.size for l in ls])
+            train_acc_sum += sum([(y_hat.argmax(axis=1) == y).sum().asscalar()
+                                 for y_hat, y in zip(y_hats, ys)])
+            m += sum([y.size for y in ys])
+        test_acc = evaluate_accuracy(test_iter, net, ctx)
+        print('epoch %d, loss %.4f, train acc %.3f, test acc %.3f, '
+              'time %.1f sec'
+              % (epoch + 1, train_l_sum / n, train_acc_sum / m, test_acc,
+                 time.time() - start))
+
+
+def train_with_data_aug(train_augs, test_augs, lr=0.001):
+    batch_size, ctx, net = 256, try_all_gpus(), d2l.resnet18(10)
+    net.initialize(ctx=ctx, init=init.Xavier())
+    trainer = gluon.Trainer(net.collect_params(), 'adam',
+                            {'learning_rate': lr})
+    loss = gloss.SoftmaxCrossEntropyLoss()
+    train_iter = load_cifar10(True, train_augs, batch_size)
+    test_iter = load_cifar10(False, test_augs, batch_size)
+    train(train_iter, test_iter, net, loss, trainer, ctx, num_epochs=10)
+
+
+train_with_data_aug(flip_aug, no_aug)
